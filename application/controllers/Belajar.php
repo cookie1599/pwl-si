ab654a027d38b2f60d3b2b0f42fd058e6bfb0d72
cookie1599 <atrisaendya@gmail.com>
2021-04-24 17:38:02 +0700

    pengenalan CI, membuat controller, membuat view

---
+++ application/controllers/Belajar.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Belajar extends CI_Controller
+{
+    public function helo()
+    {
+        $this->load->view('belajar_view');
+    }
+}
